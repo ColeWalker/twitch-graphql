fdce538f83a2971d1967116be720cf952c9dd085
ColeWalker <colegrwalker@gmail.com>
2020-08-30 16:03:27 -0400

    fix: :fire: remove dist directory

--- dist/schema/game-type.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.typeDef = void 0;
-exports.typeDef = "\ntype Game {\n    boxArtUrl: String\n    id: String\n    name: String\n\n}\n\n";
--- dist/schema/subscriber-type.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.typeDef = void 0;
-exports.typeDef = "\ntype Subscriber {\n    cumulativeMonths: Int\n    tier: Int\n    userDisplayName: String\n    userId: String\n    isGift: Boolean\n    user: User\n}\n\n";
