e1ab011137661860c6a2cd03c687940bda48edc7
ColeWalker <colegrwalker@gmail.com>
2020-09-05 12:02:48 -0400

    feat: :sparkles: add stream type schema

--- src/schema/stream-type-schema.ts
+++ src/schema/stream-type-schema.ts
@@ -1,14 +1,63 @@
-export const StreamSchema = `
-type Stream implements Node {
-  language: String
-  gameId: String
-  id: String
-  title: String
-  viewers: Int
-  thumbnailUrl: String
-  userDisplayName: String
-  userId: String
+import { createModule, gql } from 'graphql-modules'
+import { HelixUser, HelixStream } from 'twitch/lib'
 
-  user: User
+export const StreamResolvers = {
+  User: {
+    async stream(user: HelixUser) {
+      return await user.getStream()
+    },
+  },
+  Stream: {
+    language(stream: HelixStream) {
+      return stream.language
+    },
+    gameId(stream: HelixStream) {
+      return stream.gameId
+    },
+    id(stream: HelixStream) {
+      return stream.id
+    },
+    title(stream: HelixStream) {
+      return stream.title
+    },
+    viewers(stream: HelixStream) {
+      return stream.viewers
+    },
+    thumbnailUrl(stream: HelixStream) {
+      return stream.thumbnailUrl
+    },
+    userDisplayName(stream: HelixStream) {
+      return stream.userDisplayName
+    },
+    userId(stream: HelixStream) {
+      return stream.userId
+    },
+    async user(stream: HelixStream) {
+      return await stream.getUser()
+    },
+  },
 }
-`
+
+export const StreamModule = createModule({
+  id: `stream-module`,
+  dirname: __dirname,
+  typeDefs: gql`
+    type Stream {
+      language: String!
+      gameId: String!
+      id: String!
+      title: String!
+      viewers: Int!
+      thumbnailUrl: String!
+      userDisplayName: String!
+      userId: String!
+
+      user: User
+    }
+
+    extend type User {
+      stream: Stream
+    }
+  `,
+  resolvers: StreamResolvers,
+})
