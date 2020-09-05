3657e48183c3dc64f23420a88c26dc04880131d2
ColeWalker <colegrwalker@gmail.com>
2020-09-05 11:19:26 -0400

    fix: :bug: Latest sub will actually return latest sub now

--- src/subscriptions/GetLatestSub.ts
+++ src/subscriptions/GetLatestSub.ts
@@ -1,12 +1,21 @@
-import ApiClient, { HelixSubscription } from 'twitch'
+import ApiClient, { HelixSubscription, TwitchAPICallType } from 'twitch'
 
 export const getLatestSub = async (
   userId: string,
   twitchClient: ApiClient
 ): Promise<HelixSubscription> => {
-  const subscriptions = await twitchClient.helix.subscriptions.getSubscriptions(
+  const subscriptions = await twitchClient.callAPI({
+    url: `channels/${userId}/subscriptions?limit=1&direction=desc`,
+    type: TwitchAPICallType.Kraken,
+  })
+
+  const allSubs = await twitchClient.helix.subscriptions.getSubscriptions(
     userId
   )
 
-  return subscriptions.data[subscriptions.data.length - 1]
+  const sub = allSubs.data.find(
+    (h) => h.userId === subscriptions.subscriptions[0].user._id
+  )
+
+  return sub || allSubs.data[allSubs.data.length - 1]
 }
