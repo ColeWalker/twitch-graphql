0125ad44a36df2fe9be5c60c145215897bc1425c
ColeWalker <colegrwalker@gmail.com>
2020-09-12 10:12:16 -0400

    feat: :sparkles: add game search by name

--- src/schema/game-type-schema.ts
+++ src/schema/game-type-schema.ts
@@ -1,10 +1,25 @@
 import { createModule, gql } from 'graphql-modules'
 import { HelixStream, HelixGame } from 'twitch/lib'
+import { TwitchClients } from '../injections/Twitch-Clients'
+import { TwitchId } from '../injections/Twitch-Id'
+import { UserId } from '../injections/User-Id'
 
 export const GameResolvers = {
   Stream: {
     async game(stream: HelixStream) {
       return await stream.getGame()
+    },
+  },
+  Query: {
+    async getGameByName(
+      _parent: {},
+      args: { gameName: string },
+      { injector }: GraphQLModules.ModuleContext
+    ) {
+      const clients = injector.get(TwitchClients)
+      const apiClient = await clients.apiClient()
+
+      return await apiClient.helix.games.getGameByName(args.gameName)
     },
   },
   Game: {
@@ -30,11 +45,16 @@ export const GameSchema = gql`
   extend type Stream {
     game: Game
   }
+
+  extend type Query {
+    getGameByName(gameName: String!): Game
+  }
 `
 
 export const GameModule = createModule({
   id: `game-module`,
   dirname: __dirname,
+  providers: [TwitchClients, TwitchId, UserId],
   typeDefs: GameSchema,
   resolvers: GameResolvers,
 })
