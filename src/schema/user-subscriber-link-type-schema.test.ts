efcb21b4bb7960fab34732f7c140fe61893f1bb7
Cole Walker <colegrwalker@gmail.com>
2020-09-19 10:41:10 -0400

    feat: :sparkles: add isSubscribedTo queries to user type in link

--- src/schema/user-subscriber-link-type-schema.test.ts
+++ src/schema/user-subscriber-link-type-schema.test.ts
@@ -45,4 +45,202 @@ describe('UserSubscriberLinkModule', () => {
     expect(user).toHaveProperty('views')
     expect(user).toHaveProperty('description')
   })
+  it('getSubscriptionToId', async () => {
+    const app = createApplication({
+      modules: [
+        QueryModule,
+        SubscriberModule,
+        UserSubscriberLinkModule,
+        UserModule,
+      ],
+    })
+    const schema = app.createSchemaForApollo()
+
+    const userId = '23573216'
+
+    const document = parse(`
+      {
+        latestSub {
+          user{
+            displayName
+            description
+            id
+            profilePictureURL
+            views
+
+            getSubscriptionToId(userId: "${userId}"){
+              userId
+              tier
+              userDisplayName
+              isGift
+            }
+          }
+        }
+      }
+    `)
+    const contextValue = { request: {}, response: {} }
+    const result = await execute({
+      schema,
+      contextValue,
+      document,
+    })
+
+    expect(result?.errors?.length).toBeFalsy()
+    const user = result?.data?.latestSub?.user
+    if (user) {
+      expect(user).toBeTruthy()
+
+      const sub = user?.getSubscriptionToId
+
+      if (sub) {
+        expect(sub).toHaveProperty('tier')
+        expect(sub).toHaveProperty('userId')
+        expect(sub).toHaveProperty('isGift')
+        expect(sub).toHaveProperty('userDisplayName')
+      }
+    }
+  })
+  it('getSubscriptionToDisplayName', async () => {
+    const app = createApplication({
+      modules: [
+        QueryModule,
+        SubscriberModule,
+        UserSubscriberLinkModule,
+        UserModule,
+      ],
+    })
+    const schema = app.createSchemaForApollo()
+
+    const displayName = 'SupCole'
+
+    const document = parse(`
+      {
+        latestSub {
+          user{
+            displayName
+            description
+            id
+            profilePictureURL
+            views
+
+            getSubscriptionToDisplayName(displayName: "${displayName}"){
+              userId
+              tier
+              userDisplayName
+              isGift
+            }
+          }
+        }
+      }
+    `)
+    const contextValue = { request: {}, response: {} }
+    const result = await execute({
+      schema,
+      contextValue,
+      document,
+    })
+
+    expect(result?.errors?.length).toBeFalsy()
+    const user = result?.data?.latestSub?.user
+    if (user) {
+      expect(user).toBeTruthy()
+
+      const sub = user?.getSubscriptionToDisplayName
+
+      if (sub) {
+        expect(sub).toHaveProperty('tier')
+        expect(sub).toHaveProperty('userId')
+        expect(sub).toHaveProperty('isGift')
+        expect(sub).toHaveProperty('userDisplayName')
+      }
+    }
+  })
+  it('isSubscribedToId', async () => {
+    const app = createApplication({
+      modules: [
+        QueryModule,
+        SubscriberModule,
+        UserSubscriberLinkModule,
+        UserModule,
+      ],
+    })
+    const schema = app.createSchemaForApollo()
+
+    const userId = '23573216'
+
+    const document = parse(`
+      {
+        latestSub {
+          user{
+            displayName
+            description
+            id
+            profilePictureURL
+            views
+
+            isSubscribedToId(userId: "${userId}")
+          }
+        }
+      }
+    `)
+    const contextValue = { request: {}, response: {} }
+    const result = await execute({
+      schema,
+      contextValue,
+      document,
+    })
+
+    expect(result?.errors?.length).toBeFalsy()
+    const user = result?.data?.latestSub?.user
+    if (user) {
+      expect(user).toBeTruthy()
+
+      const sub = user?.isSubscribedToId
+      expect(typeof sub).toEqual('boolean')
+    }
+  })
+  it('isSubscribedToDisplayName', async () => {
+    const app = createApplication({
+      modules: [
+        QueryModule,
+        SubscriberModule,
+        UserSubscriberLinkModule,
+        UserModule,
+      ],
+    })
+    const schema = app.createSchemaForApollo()
+
+    const displayName = 'SupCole'
+
+    const document = parse(`
+      {
+        latestSub {
+          user{
+            displayName
+            description
+            id
+            profilePictureURL
+            views
+
+            isSubscribedToDisplayName(displayName: "${displayName}")
+          }
+        }
+      }
+    `)
+    const contextValue = { request: {}, response: {} }
+    const result = await execute({
+      schema,
+      contextValue,
+      document,
+    })
+
+    expect(result?.errors?.length).toBeFalsy()
+    const user = result?.data?.latestSub?.user
+    if (user) {
+      expect(user).toBeTruthy()
+
+      const sub = user?.isSubscribedToDisplayName
+      expect(typeof sub).toEqual('boolean')
+    }
+  })
 })
