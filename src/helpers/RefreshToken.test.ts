f635102d89b44cda66de3790c4ffd1e2ee1acf9c
CodeItQuick <evanontario@hotmail.com>
2020-10-10 15:41:48 -0300

    fix: added additional assert

--- src/helpers/RefreshToken.test.ts
+++ src/helpers/RefreshToken.test.ts
@@ -1,24 +1,25 @@
-import RefreshToken from './RefreshToken';
-import nock from 'nock';
-import axios from 'axios';
+import RefreshToken from './RefreshToken'
+import nock from 'nock'
+import axios from 'axios'
 
-require('dotenv').config();
-const clientId = 'user_id';
-const clientSecret = 'client_secret';
-const refreshToken = 'refresh_token';
+require('dotenv').config()
+const clientId = 'user_id'
+const clientSecret = 'client_secret'
+const refreshToken = 'refresh_token'
 
-axios.defaults.adapter = require('axios/lib/adapters/http');
+axios.defaults.adapter = require('axios/lib/adapters/http')
 describe('RefreshToken', () => {
-	it('Gets auth correctly', async () => {
-		const scope = nock('https://id.twitch.tv')
-			.post(
-				`/oauth2/token?grant_type=refresh_token&refresh_token=${refreshToken}&client_id=${clientId}&client_secret=${clientSecret}`
-			)
-			.reply(200, {
-				access_token: 'dummy_string'
-			})
-			.persist();
-		const auth = await RefreshToken({ refreshToken, clientId, clientSecret });
-		expect(auth).toHaveProperty('clientId');
-	});
-});
+  it('Gets auth correctly', async () => {
+    const scope = nock('https://id.twitch.tv')
+      .post(
+        `/oauth2/token?grant_type=refresh_token&refresh_token=${refreshToken}&client_id=${clientId}&client_secret=${clientSecret}`
+      )
+      .reply(200, {
+        access_token: 'dummy_string',
+      })
+      .persist()
+    const auth = await RefreshToken({ refreshToken, clientId, clientSecret })
+    expect(auth).toHaveProperty('clientId')
+    expect(auth.clientId).toBe('user_id')
+  })
+})
--- src/helpers/RefreshToken.ts
+++ src/helpers/RefreshToken.ts
@@ -1,26 +1,31 @@
-import axios from 'axios';
-import { AuthProvider, StaticAuthProvider } from 'twitch/lib';
-import { error } from 'console';
-require('dotenv').config();
+import axios from 'axios'
+import { AuthProvider, StaticAuthProvider } from 'twitch/lib'
+import { error } from 'console'
+require('dotenv').config()
 
-const client_id = process.env.USER_ID || '';
-const client_secret = process.env.SECRET || '';
-const refresh_token = process.env.REFRESH_TOKEN || '';
+const client_id = process.env.USER_ID || ''
+const client_secret = process.env.SECRET || ''
+const refresh_token = process.env.REFRESH_TOKEN || ''
 
 export default async ({
-	clientId = client_id,
-	clientSecret = client_secret,
-	refreshToken = refresh_token
+  clientId = client_id,
+  clientSecret = client_secret,
+  refreshToken = refresh_token,
 }): Promise<AuthProvider> => {
-	if (!clientId.length || !clientSecret.length || !refreshToken.length) {
-		throw error('env not loading properly');
-	}
-	const raw = await axios.post(
-		`https://id.twitch.tv/oauth2/token?grant_type=refresh_token&refresh_token=${refreshToken}&client_id=${clientId}&client_secret=${clientSecret}`
-	);
+  if (!clientId.length || !clientSecret.length || !refreshToken.length) {
+    throw error('env not loading properly')
+  }
+  const raw = await axios.post(
+    `https://id.twitch.tv/oauth2/token?grant_type=refresh_token&refresh_token=${refreshToken}&client_id=${clientId}&client_secret=${clientSecret}`
+  )
 
-	const accessToken = raw.data.access_token;
-	const authProvider: StaticAuthProvider = new StaticAuthProvider(clientId, accessToken);
+  const accessToken = raw.data.access_token
+  const authProvider: StaticAuthProvider = new StaticAuthProvider(
+    clientId,
+    accessToken
+  )
 
-	return authProvider;
-};
+  console.log(authProvider) //?
+
+  return authProvider
+}
