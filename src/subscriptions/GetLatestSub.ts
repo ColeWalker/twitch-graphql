70edad4e54f695ac66e5d026e80a66bdca6a5cab
Cole Walker <colegrwalker@gmail.com>
2020-10-10 10:42:54 -0400

    fix: fix minor ts warnings

--- src/subscriptions/GetLatestSub.ts
+++ src/subscriptions/GetLatestSub.ts
@@ -1,16 +1,30 @@
 import { ApiClient, HelixSubscription, TwitchAPICallType } from 'twitch'
 
 interface SubscriptionWrapper {
-  data: SubscriptionData[]
+  _total: number
+  subscriptions: SubscriptionData[]
 }
 
 interface SubscriptionData {
+  created_at: string
   broadcaster_name: string
-  is_gift: boolean
-  tier: string
-  plan_name: string
-  user_id: string
-  user_name: string
+  _id: '1e328f31280778ae3866ec93b276551582291a0d'
+  sub_plan: '1000'
+  sub_plan_name: 'Codemonkey'
+  is_gift: false
+  user: RawUser
+  sender: null
+}
+
+interface RawUser {
+  display_name: 'aussieboi'
+  type: string
+  bio: string
+  created_at: string
+  updated_at: string
+  _id: string
+  name: string
+  logo: string
 }
 
 export const getLatestSub = async (
@@ -27,7 +41,7 @@ export const getLatestSub = async (
   )
 
   const sub = helixSubs.data.find(
-    (h) => h.userId === subscriptions?.data?.[0]?.user_id
+    (h) => h.userId === subscriptions.subscriptions?.[0]?.user?._id
   )
 
   return sub || helixSubs.data[helixSubs.data.length - 1]
