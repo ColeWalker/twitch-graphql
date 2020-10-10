2a72cd10054b8e632d4ba3a3b2e782c5cfe97e61
Cole Walker <colegrwalker@gmail.com>
2020-10-10 10:25:00 -0400

    fix: update dependencies

--- src/subscriptions/GetLatestSub.ts
+++ src/subscriptions/GetLatestSub.ts
@@ -1,10 +1,23 @@
 import { ApiClient, HelixSubscription, TwitchAPICallType } from 'twitch'
 
+interface SubscriptionWrapper {
+  data: SubscriptionData[]
+}
+
+interface SubscriptionData {
+  broadcaster_name: string
+  is_gift: boolean
+  tier: string
+  plan_name: string
+  user_id: string
+  user_name: string
+}
+
 export const getLatestSub = async (
   userId: string,
   twitchClient: ApiClient
 ): Promise<HelixSubscription> => {
-  const subscriptions = await twitchClient.callApi({
+  const subscriptions: SubscriptionWrapper = await twitchClient.callApi({
     url: `channels/${userId}/subscriptions?limit=1&direction=desc`,
     type: TwitchAPICallType.Kraken,
   })
@@ -14,7 +27,7 @@ export const getLatestSub = async (
   )
 
   const sub = helixSubs.data.find(
-    (h) => h.userId === subscriptions?.subscriptions?.[0]?.user?._id
+    (h) => h.userId === subscriptions?.data?.[0]?.user_id
   )
 
   return sub || helixSubs.data[helixSubs.data.length - 1]
