a6c18f11c2528fb52b1af89702a27a386bdfdf5f
CodeItQuick <evanontario@hotmail.com>
2020-10-10 15:14:57 -0300

    fix: fix slow auth tests

--- src/helpers/RefreshToken.ts
+++ src/helpers/RefreshToken.ts
@@ -1,25 +1,26 @@
-import axios from 'axios'
-import { AuthProvider, StaticAuthProvider } from 'twitch/lib'
-import { error } from 'console'
-require('dotenv').config()
+import axios from 'axios';
+import { AuthProvider, StaticAuthProvider } from 'twitch/lib';
+import { error } from 'console';
+require('dotenv').config();
 
-export default async (): Promise<AuthProvider> => {
-  const clientId = process.env.USER_ID || ''
-  const clientSecret = process.env.SECRET || ''
-  const refreshToken = process.env.REFRESH_TOKEN || ''
+const client_id = process.env.USER_ID || '';
+const client_secret = process.env.SECRET || '';
+const refresh_token = process.env.REFRESH_TOKEN || '';
 
-  if (!clientId.length || !clientSecret.length || !refreshToken.length) {
-    throw error('env not loading properly')
-  }
-  const raw = await axios.post(
-    `https://id.twitch.tv/oauth2/token?grant_type=refresh_token&refresh_token=${refreshToken}&client_id=${clientId}&client_secret=${clientSecret}`
-  )
+export default async ({
+	clientId = client_id,
+	clientSecret = client_secret,
+	refreshToken = refresh_token
+}): Promise<AuthProvider> => {
+	if (!clientId.length || !clientSecret.length || !refreshToken.length) {
+		throw error('env not loading properly');
+	}
+	const raw = await axios.post(
+		`https://id.twitch.tv/oauth2/token?grant_type=refresh_token&refresh_token=${refreshToken}&client_id=${clientId}&client_secret=${clientSecret}`
+	);
 
-  const accessToken = raw.data.access_token
-  const authProvider: StaticAuthProvider = new StaticAuthProvider(
-    clientId,
-    accessToken
-  )
+	const accessToken = raw.data.access_token;
+	const authProvider: StaticAuthProvider = new StaticAuthProvider(clientId, accessToken);
 
-  return authProvider
-}
+	return authProvider;
+};
