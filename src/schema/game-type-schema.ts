71d474d3675be0364350b66d67b59b48acb988ef
CodeItQuick <evanontario@hotmail.com>
2020-10-10 15:22:53 -0300

    fix: extra file in here not needed that has changes

--- src/schema/game-type-schema.ts
+++ src/schema/game-type-schema.ts
@@ -1,47 +1,51 @@
-import { createModule, gql } from 'graphql-modules';
-import { HelixGame } from 'twitch/lib';
-import { TwitchClients } from '../injections/Twitch-Clients';
-import { TwitchId } from '../injections/Twitch-Id';
-import { UserId } from '../injections/User-Id';
+import { createModule, gql } from 'graphql-modules'
+import { HelixGame } from 'twitch/lib'
+import { TwitchClients } from '../injections/Twitch-Clients'
+import { TwitchId } from '../injections/Twitch-Id'
+import { UserId } from '../injections/User-Id'
 
 export const GameResolvers = {
-	Query: {
-		async getGameByName(_parent: {}, args: { gameName: string }, { injector }: GraphQLModules.ModuleContext) {
-			const clients = injector.get(TwitchClients);
-			const apiClient = await clients.apiClient();
+  Query: {
+    async getGameByName(
+      _parent: {},
+      args: { gameName: string },
+      { injector }: GraphQLModules.ModuleContext
+    ) {
+      const clients = injector.get(TwitchClients)
+      const apiClient = await clients.apiClient()
 
-			return await apiClient.helix.games.getGameByName(args.gameName);
-		}
-	},
-	Game: {
-		id(game: HelixGame) {
-			return game.id;
-		},
-		name(game: HelixGame) {
-			return game.name;
-		},
-		boxArtUrl(game: HelixGame) {
-			return game.boxArtUrl;
-		}
-	}
-};
+      return await apiClient.helix.games.getGameByName(args.gameName)
+    },
+  },
+  Game: {
+    id(game: HelixGame) {
+      return game.id
+    },
+    name(game: HelixGame) {
+      return game.name
+    },
+    boxArtUrl(game: HelixGame) {
+      return game.boxArtUrl
+    },
+  },
+}
 
 export const GameSchema = gql`
-	type Game {
-		boxArtUrl: String!
-		id: String!
-		name: String!
-	}
+  type Game {
+    boxArtUrl: String!
+    id: String!
+    name: String!
+  }
 
-	extend type Query {
-		getGameByName(gameName: String!): Game
-	}
-`;
+  extend type Query {
+    getGameByName(gameName: String!): Game
+  }
+`
 
 export const GameModule = createModule({
-	id: `game-module`,
-	dirname: __dirname,
-	providers: [ TwitchClients, TwitchId, UserId ],
-	typeDefs: GameSchema,
-	resolvers: GameResolvers
-});
+  id: `game-module`,
+  dirname: __dirname,
+  providers: [TwitchClients, TwitchId, UserId],
+  typeDefs: GameSchema,
+  resolvers: GameResolvers,
+})
